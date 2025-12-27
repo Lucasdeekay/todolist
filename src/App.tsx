01e87015472f954c92bb5b4bcb31b7a3bc4e654d
Lucasdeekay <lucasdeekay98@gmail.com>
2025-12-27 19:24:43 +0100

    My first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import './App.css';
+
+
+function App() {
+
+  return (
+    <>
+      <div className="container mx-auto">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="text-xl">
+      <span className="font-black">TaskFlow:</span>
+      <span className="font-bold">Your Creative To-do List</span>
+    </div>
+  );
+}
+
+export default Header
